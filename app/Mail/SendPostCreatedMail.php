af388e0098281dc2faec2f80bc9204fae2984fad
Ste-cracco <s.cracco@yahoo.it>
2022-10-27 16:35:45 +0200

    Esercizio Completato + Bonus

--- app/Mail/SendPostCreatedMail.php
+++ app/Mail/SendPostCreatedMail.php
@@ -6,6 +6,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
+use App\Post;
 
 class SendPostCreatedMail extends Mailable
 {
@@ -18,7 +19,7 @@ class SendPostCreatedMail extends Mailable
      *
      * @return void
      */
-    public function __construct(Post $post)
+    public function __construct(Post $post) // Importare use App\Post;
     {
         $this->post = $post;
     }
@@ -28,8 +29,12 @@ class SendPostCreatedMail extends Mailable
      *
      * @return $this
      */
-    public function build(Post $post)
-    {
-        return $this->view('mails.post_created', $post);
+    public function build()
+    {   
+        // Senza Markdown
+        // return $this->view('mails.post_created');
+
+        // Con Markdown
+        return $this->markdown('mails.post_created');
     }
 }
--- resources/views/mails/post_created.blade.php
+++ resources/views/mails/post_created.blade.php
@@ -1,6 +1,24 @@
-<label for="">Titolo del nuovo Post</label>
+@component('mail::message')
+# Nuovo Post
+
+Un nuovo Post è stato creato!
+ 
+@component('mail::button', ['url' => route('admin.post.show', $post)])
+{{ $post->title }}
+@endcomponent
+ 
+Grazie,<br>
+{{ config('app.name') }}
+@endcomponent
+
+
+
+
+{{-- Senza Markup
+    
+    <label for="">Titolo del nuovo Post</label>
 
 <h1>
     <a href="{{ route('admin.post.show', $post) }}"></a>
     {{ $post->title }}
-</h1>
+</h1> --}}
